60e26f09a335bed34cd14d215e1c54f8dcfd6a99
Sam Farndale <sam.farndale@autotrader.co.uk>
2021-12-02 13:26:40 +0000

    Update file structure

--- src/solutions/Day01.kt
+++ src/solutions/Day01.kt
@@ -1,20 +1,21 @@
-fun main() {
-    fun part1(input: List<String>): Int {
+package solutions
 
+import readInput
+
+fun main() {
+    fun part1(input: List<Int>): Int {
         var result = 0
 
-        (input.map { it.toInt() }).indices.forEach {
+        input.indices.forEach {
             if (it != 0 && input[it] > input[it - 1]) result++
         }
 
         return result
     }
 
-    fun part2(input: List<String>): Int {
-
+    fun part2(input: List<Int>): Int {
         var result = 0
-
-        val sum = input.windowed(3, 1).map { it -> it.sumOf { it.toInt() } }
+        val sum = input.windowed(3, 1).map { it.sum() }
 
         sum.indices.forEach {
             if (it != 0 && sum[it] > sum[it - 1]) result++
@@ -23,7 +24,7 @@ fun main() {
         return result
     }
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
 }
