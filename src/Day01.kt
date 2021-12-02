478d75b85042fba36c3b8dd16b6237ac87fadbdb
Sam Farndale <sam.farndale@autotrader.co.uk>
2021-12-02 12:54:18 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var result = 0
+
+        (input.map { it.toInt() }).indices.forEach {
+            if (it != 0 && input[it] > input[it - 1]) result++
+        }
+
+        return result
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        var result = 0
+
+        val sum = input.windowed(3, 1).map { it -> it.sumOf { it.toInt() } }
+
+        sum.indices.forEach {
+            if (it != 0 && sum[it] > sum[it - 1]) result++
+        }
+
+        return result
+    }
 
     val input = readInput("Day01")
     println(part1(input))
